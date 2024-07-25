fca686cad4912a6d7c8266e89bb803636579bfa5
Silvio <silviozv.oli@gmail.com>
2024-07-24 21:25:40 -0300

    📚docs (Start): Documentação.

--- PBL/src/start/__main__.py
+++ PBL/src/start/__main__.py
@@ -1,5 +1,13 @@
+"""
+Módulo de inicialização do relógio.
+"""
+
 import __init__
 from api.API import start
 
 if __name__=="__main__":
+    """
+    Inicialização do relógio através da função start.
+    """
+
     start()
